bb497a8618b0fece0fd16d02ed1dd9951d558d25
Khanh2110 <18520402@gm.uit.edu.vn>
2021-12-10 19:48:02 +0700

    add file medicine.cs

---
+++ FinalProject/FinalProject/FinalProject/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace FinalProject
+{
+    public class Medicine
+    {
+        [PrimaryKey, AutoIncrement]
+        public int MedicineID { get; set; }
+        public string MedicineName { get; set; } // Tên thuốc
+        public string Date { get; set; } // Ngày bắt đầu nhắc nhở uống thuốc
+        public string Number { get; set; } //Liều lượng thuốc
+        public string Unit { get; set; } // Đơn vị liều
+        public string Time { get; set; } // Thời gian nhắc nhở
+    }
+}
